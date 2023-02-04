77116fb2970bbb301a39f3713be2ba431291b035
Lukáš Kurčík <lukas.kurcik@gmail.com>
2023-02-04 17:03:34 +0100

    Remember localization

--- src/contexts/LocalizationProvider.tsx
+++ src/contexts/LocalizationProvider.tsx
@@ -8,16 +8,29 @@ export interface ILocalizationContext {
 
 export const LocalizationContext = React.createContext<ILocalizationContext>({
     localization: Localization.English,
-    setLocalization: () => {}
+    setLocalization: () => {
+    }
 })
 
 type Props = {
     children: ReactNode,
 };
 
+export const LOCALIZATION_KEY = 'localization';
+
 export const LocalizationProvider: React.FunctionComponent<Props> = React.memo(( {children} ) => {
-    const [localization, setLocalization] = React.useState(Localization.English);
-    const value = React.useMemo(() => ({localization, setLocalization}), [localization]);
+    const [localization, setLocalization] = React.useState<Localization>(() => {
+        const localLocalization = parseInt(localStorage.getItem(LOCALIZATION_KEY) ?? '1');
+        return (!isNaN(localLocalization) && localLocalization) ? (localLocalization as Localization) : Localization.English;
+    });
+    const setLocalizationCallback = React.useCallback(( localization: Localization ) => {
+        localStorage.setItem(LOCALIZATION_KEY, JSON.stringify(localization));
+        setLocalization(localization);
+    }, [setLocalization]);
+    const value = React.useMemo(() => ({
+        localization,
+        setLocalization: setLocalizationCallback
+    }), [localization, setLocalizationCallback]);
     return <LocalizationContext.Provider value={value}>{children}</LocalizationContext.Provider>
 });
 export const useLocalizationContext = () => React.useContext(LocalizationContext);
